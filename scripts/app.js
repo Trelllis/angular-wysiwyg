20ff195fead62158377488a812f3c2c6d44506f0
Amine <amineatallah@gmail.com>
2015-10-07 15:57:27 +0300

    testing

--- scripts/app.js
+++ scripts/app.js
@@ -6,15 +6,12 @@ app.controller('MyCtrl', function($scope) {
     }
     $scope.disabled = false;
     $scope.menu = [
-        ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript'],
+        ['bold', 'underline'],
         ['format-block'],
-        ['font'],
-        ['font-size'],
-        ['font-color', 'hilite-color'],
         ['remove-format'],
         ['ordered-list', 'unordered-list', 'outdent', 'indent'],
         ['left-justify', 'center-justify', 'right-justify'],
-        ['code', 'quote', 'paragraph'],
+        ['quote', 'paragraph'],
         ['link', 'image'],
         ['css-class']
     ];
