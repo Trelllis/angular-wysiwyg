c4e7aa1460cd433e7b1506f8d0c32befc34ef931
Amine <amineatallah@gmail.com>
2016-01-13 23:36:01 +0200

    remove facebook video width

--- dist/angular-wysiwyg.js
+++ dist/angular-wysiwyg.js
@@ -737,7 +737,7 @@ Requires:
                     el.setAttribute('data-link', facebookUrl);
                     el.classList.add('facebook_embed_wrapper');
 
-                    el.innerHTML = '<div class="fb-video" data-href="' + facebookUrl + '" data-allowfullscreen="true" data-width="600"></div>';
+                    el.innerHTML = '<div class="fb-video" data-href="' + facebookUrl + '" data-allowfullscreen="true"></div>';
                     range.insertNode(el);
 
                     if (el.parentNode.getAttribute('id') === "question") {
