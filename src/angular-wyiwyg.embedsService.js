63d78dd14f188363d27db3ec8aa5362e61af5801
Amine <amineatallah@gmail.com>
2016-01-19 13:01:52 +0200

    support https

--- src/angular-wyiwyg.embedsService.js
+++ src/angular-wyiwyg.embedsService.js
@@ -12,14 +12,14 @@
     }
 
     function getInstagramEmbed(url){
-      return $http.jsonp('http://api.instagram.com/oembed?url=' + url + '&omitscript=true&callback=JSON_CALLBACK')
+      return $http.jsonp('//api.instagram.com/oembed?url=' + url + '&omitscript=true&callback=JSON_CALLBACK')
             .then(function(response){
               return response.data;
             });
     }
 
     function getTwitterEmbed(url){
-      return $http.jsonp('https://api.twitter.com/1/statuses/oembed.json?url=' + url + '&omit_script=true&lang=ar&callback=JSON_CALLBACK')
+      return $http.jsonp('//api.twitter.com/1/statuses/oembed.json?url=' + url + '&omit_script=true&lang=ar&callback=JSON_CALLBACK')
               .then(function (response) {
                 return response.data;
               });
